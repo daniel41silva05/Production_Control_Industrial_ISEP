ac9a460c0550daac2fed32bb0ad6ea71d6afcca1
daniels41 <dsilva260405@gmail.com>
2025-02-28 00:27:07 +0000

    Implementation of methods that register workstations coming from csv files

--- src/main/java/org/project/service/WorkstationService.java
+++ src/main/java/org/project/service/WorkstationService.java
@@ -4,6 +4,7 @@ import org.project.data.ConnectionFactory;
 import org.project.data.DatabaseConnection;
 import org.project.exceptions.OperationException;
 import org.project.exceptions.WorkstationException;
+import org.project.io.CsvReader;
 import org.project.model.OperationType;
 import org.project.model.Workstation;
 import org.project.model.WorkstationType;
@@ -12,7 +13,10 @@ import org.project.repository.Repositories;
 import org.project.repository.WorkstationRepository;
 import org.project.repository.WorkstationTypeRepository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class WorkstationService {
 
@@ -97,9 +101,69 @@ public class WorkstationService {
         return workstationType;
     }
 
-    // registar worstation csv
+    public Map<OperationType, List<WorkstationType>> registerWorkstationTypesFromCSV (String filePath) throws WorkstationException, OperationException {
+        Map<OperationType, List<WorkstationType>> operationWorkstationsMap = new HashMap<>();
+        Map<WorkstationType, Map<Integer, Integer>> workstationOperationTimeMap = CsvReader.loadWorkstationTypes(filePath);
 
-    // registar workstationtype csv
+        boolean success = false;
+
+        for (Map.Entry<WorkstationType, Map<Integer, Integer>> entry : workstationOperationTimeMap.entrySet()) {
+            WorkstationType workstationType = entry.getKey();
+            Map<Integer, Integer> operationTimeMap = entry.getValue();
+
+            if (!workstationTypeRepository.getWorkstationTypeExists(connection, workstationType.getId())) {
+                workstationTypeRepository.save(connection, workstationType);
+
+                for (Map.Entry<Integer, Integer> entry2 : operationTimeMap.entrySet()) {
+                    int operationID = entry2.getKey();
+                    int setupTime = entry2.getValue();
+
+                    if (!operationTypeRepository.getOperationTypeExists(connection, operationID)) {
+                        return null;
+                    }
+                    OperationType operationType = operationTypeRepository.getById(connection, operationID);
+                    operationType.getWorkstationSetupTime().put(workstationType, setupTime);
+
+                    success = workstationTypeRepository.saveOperationWorkstationTime(connection, operationType, workstationType);
+                    if (!success) {
+                        return null;
+                    }
+                    operationWorkstationsMap.computeIfAbsent(operationType, k -> new ArrayList<>()).add(workstationType);
+                }
+
+            } else {
+                workstationType = workstationTypeRepository.getById(connection, workstationType.getId());
+
+                for (Map.Entry<Integer, Integer> entry2 : operationTimeMap.entrySet()) {
+                    int operationID = entry2.getKey();
+                    int setupTime = entry2.getValue();
+
+                    if (!operationTypeRepository.getOperationTypeExists(connection, operationID)) {
+                        return null;
+                    }
+                    OperationType operationType = operationTypeRepository.getById(connection, operationID);
+
+                    if (operationType.getWorkstationSetupTime().containsKey(workstationType)) {
+                        if (operationType.getWorkstationSetupTime().get(workstationType) != setupTime) {
+                            operationType.getWorkstationSetupTime().put(workstationType, setupTime);
+                            success = workstationTypeRepository.updateOperationWorkstationTime(connection, operationType, workstationType);
+                        }
+                    } else {
+                        operationType.getWorkstationSetupTime().put(workstationType, setupTime);
+                        success = workstationTypeRepository.saveOperationWorkstationTime(connection, operationType, workstationType);
+                    }
+
+                    if (!success) {
+                        return null;
+                    }
+                    operationWorkstationsMap.computeIfAbsent(operationType, k -> new ArrayList<>()).add(workstationType);
+                }
+            }
+
+        }
+
+        return  operationWorkstationsMap;
+    }
 
     public Integer changeSetupTime (int operationTypeId, int workstationTypeId, int newSetupTime) throws WorkstationException, OperationException {
         if (!operationTypeRepository.getOperationTypeExists(connection, operationTypeId)) {
