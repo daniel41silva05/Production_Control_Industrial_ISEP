213bde432fe4ef3a1a2425627e49b3e2647eac67
daniels41 <dsilva260405@gmail.com>
2025-03-04 01:06:55 +0000

    Controllers of operations involving supply offers (register, delete, update, consult actives, status)

---
+++ src/main/java/org/project/controller/SupplyOfferController.java
@@ -0,0 +1,45 @@
+package org.project.controller;
+
+import org.project.exceptions.ProductException;
+import org.project.exceptions.SupplierException;
+import org.project.exceptions.SupplyOfferException;
+import org.project.model.ProcessState;
+import org.project.model.SupplyOffer;
+import org.project.service.SupplyOfferService;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+public class SupplyOfferController {
+
+    private SupplyOfferService supplyOfferService;
+
+    public SupplyOfferController() {
+        supplyOfferService = new SupplyOfferService();
+    }
+
+    public SupplyOffer getSupplyOfferByID (int id) throws SupplyOfferException {
+        return supplyOfferService.getSupplyOfferByID (id);
+    }
+
+    public SupplyOffer registerSupplyOffer(int supplierID, int supplyOfferID, String deliveryStreet, String deliveryZipCode, String deliveryTown, String deliveryCountry, Date startDate, Date endDate, ProcessState state, Map<String, Map<Integer, Double>> rawMaterialIDsQuantityCost) throws SupplierException, SupplyOfferException, ProductException {
+        return supplyOfferService.registerSupplyOffer(supplierID, supplyOfferID, deliveryStreet, deliveryZipCode, deliveryTown, deliveryCountry, startDate, endDate, state, rawMaterialIDsQuantityCost);
+    }
+
+    public SupplyOffer deleteSupplyOffer (int id) throws SupplyOfferException {
+        return supplyOfferService.deleteSupplyOffer(id);
+    }
+
+    public SupplyOffer updateSupplyOffer (SupplyOffer supplyOffer, String deliveryStreet, String deliveryZipCode, String deliveryTown, String deliveryCountry, Date startDate, Date endDate) throws SupplyOfferException {
+        return supplyOfferService.updateSupplyOffer(supplyOffer, deliveryStreet, deliveryZipCode, deliveryTown, deliveryCountry, startDate, endDate);
+    }
+
+    public List<SupplyOffer> activeSupplyOffers () {
+        return supplyOfferService.activeSupplyOffers();
+    }
+
+    public SupplyOffer completeSupplyOffer(int supplyOfferID) throws SupplyOfferException, ProductException {
+        return supplyOfferService.completeSupplyOffer(supplyOfferID);
+    }
+}
